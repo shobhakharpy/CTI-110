4213e1294964f0e22966e720d605787eca99254d
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-02-15 15:47:30 -0500

    Add files via upload

---
+++ P2HW1_DistanceTraveled_AdhikariShobhakhar.py
@@ -0,0 +1,22 @@
+# CTI-110
+# P2HW1 - Distance Traveled
+# Shobhkhar Adhikari
+# 2/12/2018
+
+
+# Enter speed.
+speed = 70
+
+# calcualte distance traveled.
+
+distanceAfterT6 = speed * 6
+distanceAfterT10 = speed * 10
+distanceAfterT15 = speed * 15
+
+# Display distance traveled.
+
+print('The distance the car will travel in 6 hours is',distanceAfterT6,'miles.')
+
+print('The distance the car will travel in 10 hours is',distanceAfterT10,'miles.')
+
+print('The distance the car will travel in 15 hours is',distanceAfterT15,'miles.')
