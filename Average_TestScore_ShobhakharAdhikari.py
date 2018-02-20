150b6def1782e272f8f10621ac5dccfeb63c9ae7
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-02-20 09:45:21 -0500

    Add files via upload

---
+++ Average_TestScore_ShobhakharAdhikari.py
@@ -0,0 +1,18 @@
+# CTI 110
+# Python 2 Exercise
+# Shobhakhar Adhikari
+# 02/20/2018
+
+# Get the test scores
+
+firstScore = float(input('Enter your first test score:'))
+secondScore = float(input('Enter your second test score:'))
+thirdScore = float(input('Enter your third test score:'))
+
+# Calculate average
+
+average = (firstScore + secondScore + thirdScore)/3
+
+
+# Display the average
+print ('The average test score is :',format(average,'.1f'))
