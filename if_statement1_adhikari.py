a04e9e765dd9a864e10d8921c2b37b2e4f6b3ba7
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-02-22 09:40:23 -0500

    Add files via upload

---
+++ if_statement1_adhikari.py
@@ -0,0 +1,20 @@
+# If Statement
+# Shobhakhar Adhikari
+# 02/ 22/ 2018
+
+# define variables and get input
+
+x = int(input('Enter the vaule of x: '))
+
+y = int(input('Enter the vaule of y: '))
+
+if x > y:
+    print('x is greater than y')
+
+print('x is less than y')
+
+# Calculations or Decisions
+
+
+
+# Output or display result
