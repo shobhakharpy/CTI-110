0337d9702604115b0bc200720049ca803524fcc4
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-02-22 09:51:12 -0500

    Add files via upload

---
+++ Bonus_Sales_PYthon3.py
@@ -0,0 +1,15 @@
+# CTI 110
+# Python 3 Exercise (Bonus-Sales)
+# Shobhakhar Adhikari
+# 02/22/2018
+
+# Get the test scores
+
+sales = float(input('Enter the total sales in dolars: $'))
+
+
+# if statement
+
+if sales > 50000:
+    print('Congratulations! your Bonus is $500')
+print()
