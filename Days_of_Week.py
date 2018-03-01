373431bad9132a9955b225d5bbb21681dcd0013e
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-03-01 17:55:47 -0500

    Add files via upload

---
+++ Days_of_Week.py
@@ -0,0 +1,28 @@
+#CTI 110
+# Shobhakhar Adhikari
+# 03/02/2018
+# Days of the Week
+
+
+# define variable and get the input
+
+x = int(input("Enter a whole number within the range of 1 trhough 7: "))
+
+# Calculations or decision structure
+
+if x == 1:
+    print("The day equivalent for the number is: Monday")
+elif x == 2:
+    print("The day equivalent for the number is: Tuesday")
+elif x == 3:
+    print("The day equivalent for the number is: Wednesday")
+elif x == 4:
+    print("The day equivalent for the number is: Thursday")
+elif x == 5:
+    print("The day equivalent for the number is: Friday")
+elif x == 6:
+    print("The day equivalent for the number is: Saturday")
+elif x == 7:
+    print("The day equivalent for the number is: Sunday")
+else:
+    print("ERROR! please enter a whole number within the range of 1 trhough 7")
