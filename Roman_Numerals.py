c3ed714758dad86ea3081fa11a09eb51f6c15ee1
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-03-01 17:40:36 -0500

    Add files via upload

---
+++ Roman_Numerals.py
@@ -0,0 +1,34 @@
+#CTI 110
+# Shobhakhar Adhikari
+# 03/02/2018
+# Roman Numerals
+
+
+# define variable and get the input
+
+x = float(input("Enter a whole number within the range of 1 trhough 10: "))
+
+# Calculations or decision structure
+
+if x == 1:
+    print("The roman numeral for the number is : I")
+elif x == 2:
+    print("The roman numeral for the number is : II")
+elif x == 3:
+    print("The roman numeral for the number is : III")
+elif x == 4:
+    print("The roman numeral for the number is : IV")
+elif x == 5:
+    print("The roman numeral for the number is : V")
+elif x == 6:
+    print("The roman numeral for the number is : VI")
+elif x == 7:
+    print("The roman numeral for the number is : VII")
+elif x == 8:
+    print("The roman numeral for the number is : VIII")
+elif x == 9:
+    print("The roman numeral for the number is : IX")
+elif x == 10:
+    print("The roman numeral for the number is : X")
+else:
+    print("ERROR! Enter a whole number within the range of 1 trhough 10")
