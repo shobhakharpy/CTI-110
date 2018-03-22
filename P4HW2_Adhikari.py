23f8c1480cd2fb4520a4b68164dc1125e35655ce
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-03-22 09:16:37 -0400

    Add files via upload

---
+++ P4HW2_Adhikari.py
@@ -0,0 +1,18 @@
+# CTI 110
+# Shobhakhar Adhikari
+# P4HW2
+# Running Total
+
+# define variable and ask user to input values
+
+total = 0
+
+
+number = 0
+
+while number >=0:
+    total += number
+    number = int(input("Enter a number: "))
+
+print("Total:", total)
+
