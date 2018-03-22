5fabab932a3a8e2fb3a6575f94e8870b33e22532
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-03-22 09:53:03 -0400

    Add files via upload

---
+++ P4T1a_Square_Loop_Adhikari.py
@@ -0,0 +1,24 @@
+# CTI 110
+# Shobhakhar Adhikari
+# P4T1a: Shapes
+
+import turtle             # Allows us to use turtles
+wn = turtle.Screen()      # Creates a playground for turtles
+sunder = turtle.Turtle()    # Create a turtle, assign to sunder
+
+# add some display options
+
+sunder.pensize(3)
+sunder.pencolor("red")
+sunder.shape("turtle")
+
+#commands from here to the last line can be replaced
+
+for x in range(4):
+
+    sunder.forward(200)          # Tell sunder to move forward by 50 units
+    sunder.left(90)             # Tell sunder to turn by 90 degrees
+
+
+# end commands
+wn.mainloop()             # Wait for user to close window
