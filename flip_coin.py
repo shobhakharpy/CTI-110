a96581a602bec32a5c7454f6bdffa1b94c2a17b5
shobhakharpy <36519546+shobhakharpy@users.noreply.github.com>
2018-04-10 19:11:13 -0400

    Add files via upload

---
+++ flip_coin.py
@@ -0,0 +1,10 @@
+import random
+def main():
+    
+    for x in range (5):
+        y = random.randint (1,2)
+        if y == 1:
+            print("Heads")
+        else:
+            print("Tails")
+main ()
